e321be03c73bddd7974011c53a1ee756c0f9d197
Your Name <you@example.com>
2019-06-13 12:20:36 -0300

    Primeiro update

---
+++ app/routes/noticia.js
@@ -0,0 +1,41 @@
+module.exports = function (application) {
+	
+	application.get('/noticia/', function(req, res){
+		//este render se refere ao arquivo /noticia/list.ejs
+		// que está dentro da pasta views			
+		res.render('noticia/list');
+	});
+
+	application.post('/noticia/list', function(req, res){
+		application.app.controllers.noticia.list(application, req, res);
+	});
+
+	application.get('/noticia/show/:noticia_id', function(req, res){
+		
+	});	
+
+	application.post('/noticia/getById', function(req, res){		
+		application.app.controllers.noticia.getById(application, req, res);
+	});	
+
+	application.get('/noticia/add', function(req, res){
+	
+	});
+		
+	application.post('/noticia/add', function(req, res){
+	
+	});
+
+	application.get('/noticia/edit/:noticia_id', function(req, res){
+	
+	});	
+	
+	application.post('/noticia/edit', function(req, res){
+	
+	});
+
+	application.get('/noticia/delete/:noticia_id', function(req, res){
+	
+	});	
+		
+}
